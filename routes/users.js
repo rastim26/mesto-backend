1210747c09769e762d68d58250a77a5613d9734b
Rastim Safarov <rastim.safarov@appius.com>
2023-09-19 18:46:18 +0100

    feat: Add Celebrate plugin

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,35 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUserInfo,
   updateUserInfo, updateUserAvatar,
 } = require('../controllers/users');
 
-router.get('/', getUsers);
-router.get('/me', getUserInfo);
-router.get('/:userId', getUserInfo);
-router.patch('/me', updateUserInfo);
-router.patch('/me/avatar', updateUserAvatar);
+router.get('/', celebrate({
+  body: Joi.object().keys({
+
+  }),
+}), getUsers);
+router.get('/me', celebrate({
+  body: Joi.object().keys({
+
+  }),
+}), getUserInfo);
+router.get('/:userId', celebrate({
+  body: Joi.object().keys({
+
+  }),
+}), getUserInfo);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    
+  }),
+}), updateUserInfo);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+
+  }),
+}), updateUserAvatar);
 
 module.exports = router;
